29b00443e616ffd2f3642773b263cf5941042a0c
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-25 17:25:03 +0000

    complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void insertionSort(char[] characters){
+		for (int i = 1; i < characters.length; i++) {
+			char key = characters[i];
+			int index = i - 1;
+			while (index >= 0 && characters[index] > key) { //check for -1 first
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
+	}
 
 
 }
